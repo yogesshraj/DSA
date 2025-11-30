2e0378e19c3f156ed7fd7514607c9ae2975ba43f
yogesshraj <yogessh19@navgurukul.org>
2025-11-30 14:11:54 +0530

    changes

--- binary_search.py
+++ binary_search.py
@@ -17,3 +17,27 @@ def minEatingSpeed(piles, H):
 piles = [3, 6, 7, 11]
 H = 8
 print(minEatingSpeed(piles, H))   # Output: 4
+
+#Given an array of integers and an integer k, 
+# return the length of the longest subarray whose sum equals exactly k.
+
+a = [1, -1, 5, -2, 3]
+k = 3
+a = [-2, -1, 2, 1] 
+k = 1
+def how_many_sub_arr(a, k):
+    prefix = [0]
+    existence = {0:1}
+    total_sub_arr = 0
+    max_value = 0
+    for i in range(len(a)):
+        pre_sum = prefix[len(prefix)-1]+ a[i]
+        needed_value = pre_sum - k
+        if needed_value in existence:
+            max_value = max(max_value, i-(prefix.index(needed_value)-1))
+            total_sub_arr += existence[needed_value]
+        existence[pre_sum] = existence.get(pre_sum, 0) + 1
+        prefix.append(pre_sum)
+
+    return max_value
+print(how_many_sub_arr(a, k))
